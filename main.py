5585ddcc7dbaa4bdcc4fe58fe2f8fe1f15497878
joe <j29scott@uwaterloo.ca>
2020-08-18 23:22:42 -0400

    runnable

--- main.py
+++ main.py
@@ -12,7 +12,9 @@ def solve(X,Y):
         tf_directory = os.path.dirname(tf.name)
     return run_aifeynman('', tf.name, BF_try_time=30,BF_ops_file_type='19ops.txt', polyfit_deg=4, NN_epochs=4000)
 
-import numpy as np
-X = [[np.random.uniform(-10, 10), np.random.uniform(-10, 10)] for i in range(10)]
-Y = [np.sqrt(x[0] * x[0] + x[1] * x[1]) for x in X]
-print(solve(X,Y))
+# import numpy as np
+# X = [[np.random.uniform(-10, 10), np.random.uniform(-10, 10)] for i in range(100)]
+# Y = [np.sqrt(x[0] * x[0] + x[1] * x[1]) for x in X]
+# ans = solve(X,Y)[-1][-1]
+# pdb.set_trace()
+# print()
